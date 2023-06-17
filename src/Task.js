eb8c82dce8036de5e6af69b346e97c490e6d107e
PutraWiraG <putrasaya793@gmail.com>
2023-06-17 15:44:42 +0700

    First Commit

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL"; // TODO: replace this
+const studentName = "Putra Dwi Wira Gardha Yuniahans"; // TODO: replace this
+const studentId = "FE4664867"; // TODO: replace this
+
+export { url, studentName, studentId };
